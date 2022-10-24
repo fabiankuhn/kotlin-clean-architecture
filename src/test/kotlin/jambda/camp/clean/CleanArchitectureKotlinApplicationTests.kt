b66bb63433cbcb5e46e2b78ebcc2f9be98ba2af5
kufa <fabian.kuhn@zuehlke.com>
2022-10-24 11:10:10 +0200

    Init

---
+++ src/test/kotlin/jambda/camp/clean/CleanArchitectureKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package jambda.camp.clean
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CleanArchitectureKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
