3af01e7bf63b6f46677892c7356c2be383381673
kufa <fabian.kuhn@zuehlke.com>
2022-10-26 17:07:26 +0200

    Simplify stock repository and move logic to buy use case

--- src/main/kotlin/jambda/camp/clean/adapter/db/StockDto.kt
+++ src/main/kotlin/jambda/camp/clean/adapter/db/StockDto.kt
@@ -2,6 +2,7 @@ package jambda.camp.clean.adapter.db
 
 import jambda.camp.clean.domain.Stock
 import jambda.camp.clean.domain.StockAmount
+import jambda.camp.clean.domain.StockId
 import org.jetbrains.annotations.NotNull
 import javax.persistence.Entity
 import javax.persistence.Id
@@ -22,8 +23,17 @@ internal data class StockDto(
 ) {
 
     fun toDomain() = Stock(
+        id = StockId(id!!),
         product = this.product!!.toDomain(),
         amount = StockAmount(this.amount!!)
     )
+
+    companion object {
+        fun fromDomain(stock: Stock): StockDto = StockDto(
+            id = stock.id.value,
+            product = ProductDto.fromDomain(stock.product),
+            amount = stock.amount.value
+        )
+    }
 }
 
